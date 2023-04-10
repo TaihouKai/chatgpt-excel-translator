60ff24a3539037629f0b441bff48915b6db54c76
TaihouKai <pengfei.w@outlook.com>
2023-04-10 15:32:01 +0900

    catch error and retry after 10 seconds

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 import openai
 import pandas
-
+import time
 
 def read_xlsx(file: str) -> pandas.DataFrame:
     """Read an excel file and return a pandas dataframe"""
@@ -14,7 +14,9 @@ def parse_xlsx(data: pandas.DataFrame, column_excel: str, start_row_excel: int) 
     return data[column_number][start_row:].tolist()
 
 
-def chatgpt_translate(text: str, from_lang: str = "Japanese", to_lang: str = "English") -> str:
+def chatgpt_translate(
+    text: str, from_lang: str = "Japanese", to_lang: str = "English"
+) -> str:
     """Send a message to the chatbot and return the response"""
     ## Null line?
     if text is None or text == "nan" or text == "None":
@@ -25,12 +27,23 @@ def chatgpt_translate(text: str, from_lang: str = "Japanese", to_lang: str = "En
         return ""
 
     messages = [
-        {"role": "system", "content": f"You are a translator. You only receive the {from_lang} text and you must translate it into {to_lang}."},
-        {"role": "user", "content": f"{text}"}
+        {
+            "role": "system",
+            "content": f"You are a translator. You only receive the {from_lang} text and you must translate it into {to_lang}.",
+        },
+        {"role": "user", "content": f"{text}"},
     ]
-    result = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    return result["choices"][0]["message"]["content"]
-
+    result = None
+    while result is None:
+        try:
+            result = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        except Exception as e:
+            print(f"Failed due to error: \n{e}")
+            print(f"Retrying in 10 seconds...")
+            time.sleep(10)
+            pass
+    # if result is dict
+    if isinstance(result, dict):
+        return result["choices"][0]["message"]["content"]
+    else:
+        return str(result)
